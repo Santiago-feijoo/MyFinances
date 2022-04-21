ab8306b62c4c768e12d16dbaac0e8196102040c2
Santiago Feijoo <49215448+Santiago-feijoo@users.noreply.github.com>
2022-04-20 20:32:36 -0500

    Avance en la creación del registro de usuario.

--- app/src/main/java/com/example/myfinances/signin/SignInActivity.kt
+++ app/src/main/java/com/example/myfinances/signin/SignInActivity.kt
@@ -2,6 +2,7 @@ package com.example.myfinances.signin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.myfinances.R
 import com.example.myfinances.databinding.ActivitySignInBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +19,12 @@ class SignInActivity: AppCompatActivity() {
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initComponents()
+
+    }
+
+    private fun initComponents() {
+        addListOfDocumentTypes(resources.getStringArray(R.array.listOfDocumentTypes))
         initListener()
 
     }
@@ -28,6 +35,12 @@ class SignInActivity: AppCompatActivity() {
             this.finish()
 
         }
+
+    }
+
+    private fun addListOfDocumentTypes(listOfDocumentTypes: Array<String>) {
+        val documentTypeAdapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, listOfDocumentTypes)
+        binding.listTypesOfDocument.setAdapter(documentTypeAdapter)
 
     }
 
